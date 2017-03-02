4572f46b61fd9431e8c18cb4cda80e05b5fd475d
Brian Faure <bfaure23@gmail.com>
2017-03-02 16:16:36 -0500

    added .cnf parsing utilities

---
+++ Problem_1/main.py
@@ -0,0 +1,92 @@
+from __future__ import print_function # to allow for \r
+import os
+import sys
+
+# class to hold all all information required about a .cnf file
+class cnf(object):
+	def __init__(self,filename):
+		self.filename = filename
+		self.read_file()
+
+	# parses a single .cnf file
+	def read_file(self):
+		#print("Reading "+str(filename)+"...")
+
+		f = open(self.filename,"r")
+		text = f.read()
+		lines = text.split("\n")
+
+		self.clauses = [] # each element is a single clause (list of integers)
+		current_clause = [] # buffer to hold current clause being parsed
+		negated = False # set to True if we encounter an empty elem
+
+		for line in lines:
+			if line[0]=="%": break # denotes the eof
+
+			if line[0]!="c" and line[0]!="p": # skip all comment lines
+				elems = line.split(" ") # split line on spaces
+				for elem in elems: # iterate over each element
+
+					if elem=="0":
+						self.clauses.append(current_clause) # append the full clause
+						current_clause = [] # empty the read buffer
+						continue
+
+					if elem!="": # if there is a clause element
+						val = int(elem) if not negated else int(elem)*-1
+						current_clause.append(val) # add to current read buffer
+						negated = False
+
+					else: # if this is an empty elem, negate the next
+						negated = True
+		#print("Parsed "+str(len(self.clauses)+" from "+filename)
+
+# class to handle loading/accessing of .cnf data
+class data(object):
+
+	def __init__(self):
+		self.var_20 = [] # to hold all .cnf with 20 variables
+		self.var_50 = [] # to hold all .cnf with 50 variables
+		self.var_75 = [] # to hold all .cnf with 75 variables
+		self.var_100 = [] # to hold all .cnf with 100 variables
+
+		self.load_data() # load in all .cnf files
+
+	def load_data(self):
+		print("Loading data...")
+
+		parent_dir = "data/"
+		child_dirs = ["20/","50/","75/","100/"]
+
+		num_read = 0
+
+		for d in child_dirs: # iterate over all data directories
+
+			path = parent_dir+d
+			items = os.listdir(path)
+
+			for f in items:
+				print("Reading... "+str(num_read),end="\r")
+
+				if f.find(".cnf")!=-1:
+					filename = path+f 
+					cur = cnf(filename)
+
+					if d == "20/": self.var_20.append(cur)
+					if d == "50/": self.var_50.append(cur)
+					if d == "75/": self.var_75.append(cur)
+					if d == "100/": self.var_100.append(cur)
+
+					num_read+=1
+
+		print("20 Variable Equations: "+str(len(self.var_20)))
+		print("50 Variable Equations: "+str(len(self.var_50)))
+		print("75 Variable Equations: "+str(len(self.var_75)))
+		print("100 Variable Equations: "+str(len(self.var_100)))
+
+def main():
+	d = data()
+
+
+if __name__ == '__main__':
+	main()

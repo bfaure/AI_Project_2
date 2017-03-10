71a34d96b856fb91e28184dc74844966c9d2f987
nnelson16 <nn167@scarletmail.rutgers.edu>
2017-03-09 21:38:47 -0500

    finished gathering data for tsp_astar

--- Problem_5/main.py
+++ Problem_5/main.py
@@ -2,10 +2,17 @@ from __future__ import print_function
 import os
 import sys
 import random
+import time
 from math import sqrt, exp
 from copy import copy, deepcopy
 import heapq
+import re
 
+numbers = re.compile(r'(\d+)')
+def numericalSort(value):
+    parts = numbers.split(value)
+    parts[1::2] = map(int, parts[1::2])
+    return parts
 
 def generateAllFiles():
     num_cities_list = [10, 25, 50, 100]
@@ -88,6 +95,8 @@ def total_cost(tour):
     return distance
 
 def tsp_astar(citylist):
+    #number of nodes generated
+    nodes_generated = 0
     #boolean array of visited locations
     visited = [0]*len(citylist)
     #starting city is the first city in the file
@@ -97,18 +106,32 @@ def tsp_astar(citylist):
     visited[startCity.cityid] = numVisited #Mark the starting city as visited
     numVisited = numVisited + 1
     currentCity = startCity
-    while(numVisited <= len(citylist)):
+    timeout = time.time() + 60*10 #10 minute timeout
+    start = time.time()
+
+    while(numVisited <= len(citylist) and time.time() < timeout):
         for i in range(0, len(citylist)):
             #not visiting itself and city is unvisited
             if (i != currentCity.cityid  and visited[i] == 0):
                 evaluation_calculation = min_distance_unvisited(currentCity, citylist, visited) + cost_calculation(currentCity, citylist[i])
                 heapq.heappush(priorityQueue, (evaluation_calculation, citylist[i]))
         currentCity = heapq.heappop(priorityQueue)[1]
+        nodes_generated = nodes_generated + 1
         while(visited[currentCity.cityid] != 0):
             currentCity = heapq.heappop(priorityQueue)[1]
+            nodes_generated = nodes_generated + 1
         visited[currentCity.cityid] = numVisited
         numVisited = numVisited + 1
-    return visited
+
+    end = time.time()
+    solved = False
+
+    print(visited)
+
+    if (numVisited-1) == len(citylist):
+        solved = True
+    execution_time = end-start
+    return visited, solved, execution_time, nodes_generated
 
 def computeProbability(currentEnergy, newEnergy, temperature):
     if newEnergy < currentEnergy:
@@ -165,7 +188,74 @@ class city(object):
         self.x = float(x_coord)
         self.y = float(y_coord)
 
+def run_tsp_astar_trials():
+    file_list = os.listdir("data/")
+    #arrange the files in the order they are listed in the directory
+    file_list = sorted(file_list, key=numericalSort)
 
+    #make lists for all the data we need to gather
+    solution_times_raw = []
+    problems_solved_raw = []
+    cost_raw = []
+    nodes_generated = []
+    problem_sizes = [10, 25, 50, 100]
+    for f in file_list:
+        cityList = readFile("data/"+f)
+        visited, solved, execution_time, num_generated = tsp_astar(cityList)
+
+        cost_raw.append(total_cost_astar(cityList, visited))
+        problems_solved_raw.append(solved)
+        solution_times_raw.append(execution_time)
+        nodes_generated.append(num_generated)
+        print("Finished trial with: "+f)
+
+    print("Finished all trials! Writing to file...")
+
+    #Filter out values for the solutions
+    solution_times_filtered = []
+    cost_filtered = []
+    problems_solved = 0
+    for i in range(0,100):
+        if problems_solved_raw[i] == True:
+            problems_solved = problems_solved + 1
+            cost_filtered.append((cost_raw[i], problem_sizes[i/25], i%25))
+            solution_times_filtered.append((solution_times_raw[i], problem_sizes[i/25], i%25))
+
+    target = open("tsp_astar_results.txt", "w")
+    target.write("Number of problems solved: "+str(problems_solved)+"\n")
+
+    sum_cost = 0
+    for cost in cost_filtered:
+        sum_cost = sum_cost + cost[0]
+    average_solution_cost = sum_cost / len(cost_filtered)
+    target.write("Average solution cost (across 100 trials): "+str(average_solution_cost)+"\n")
+
+    sum_solution_time = 0
+    for sol in solution_times_filtered:
+        sum_solution_time = sum_solution_time + sol[0]
+    average_solution_time = sum_solution_time / len(solution_times_filtered)
+    target.write("Average solution time: "+str(average_solution_time)+"\n\n")
+
+    #Write lists to file
+    target.write("Solution cost list (cost, problemsize, trial number)\n")
+    target.write("----------------------------\n")
+    for t in cost_filtered:
+        target.write(' '.join(str(s) for s in t) + '\n')
+    target.write("\n\n")
+
+    target.write("Solution times list (time, problemsize, trial number)\n")
+    target.write("----------------------------\n")
+    for t in solution_times_filtered:
+        target.write(' '.join(str(s) for s in t) + '\n')
+    target.write("\n\n")
+
+    target.write("Nodes generated list")
+    target.write("----------------------------\n")
+    for i in range(0,100):
+        target.write("Size: "+str(problem_sizes[i/25])+" Trial: "+str(i%25)+" Generated: "+str(nodes_generated[i])+"\n")
+
+    target.close()
+    print("Done!")
 
 def main():
     #generate file here
@@ -174,7 +264,8 @@ def main():
     #order = tsp_astar(cityList)
     #print(total_cost_astar(cityList, order))
     #tsp_sa(cityList)
-    generateAllFiles()
+    #generateAllFiles()
+    run_tsp_astar_trials()
 
 if __name__ == '__main__':
 	main()
